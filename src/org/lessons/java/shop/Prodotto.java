eb7dc6c4b83c6c39f962c32b884b1787b28ae0a7
samir <samirsadriuboolean@gmail.com>
2026-01-06 12:19:06 +0100

    consegna dell'esercizio L'05

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,36 +1,85 @@
 package org.lessons.java.shop;
+
 import java.util.Random;
 
 public class Prodotto {
-    public int codex;
-    public String name;
-    public String description;
-    public float price;
-    public float iva;
+    private int codex;
+    private String name;
+    private String description;
+    private String brand;
+    private float price;
+    private float iva;
 
-    public Prodotto(String name, String description, float price, float iva ){
+    public Prodotto(String name, String description, float price, float iva) {
         Random random = new Random();
         this.codex = random.nextInt(300);
-        this.name= name;
+        this.name = name;
         this.description = description;
         this.price = price;
         this.iva = iva;
 
     }
 
-    public float getPrice(){
-        return this.price;
-    }
-    
-    public float getPriceWithIva(){
-        float PriceIva = Math.round(this.price * (1 + this.iva/100));
-        return PriceIva;
-    }
-    
-    public String getCompleteName(){
-        String completeName = this.codex + this.name;
-        return completeName;
+    public Prodotto(String name, String descrizione, String brand, float price) {
+        Random random = new Random();
+        this.codex = random.nextInt(3000);
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
     }
 
+    public int getCodex(){
+        return this.codex;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+        public float getIva() {
+        return this.iva;
+    }
+
+    public void setIva(float iva) {
+        this.iva = iva;
+    }
+    // public float getPriceWithIva(){
+    // float PriceIva = Math.round(this.price * (1 + this.iva/100));
+    // return PriceIva;
+    // }
+
+    // public String getCompleteName(){
+    // String completeName = this.codex + this.name;
+    // return completeName;
+    // }
 
 }
